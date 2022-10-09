d679ebf103de44e1f661a130630c75462ae2e85b
StBiccy <115167027+StBiccy@users.noreply.github.com>
2022-10-09 17:44:42 +0100

    program 8
    idk what changed in this one but its all cool

--- Project8_pi/Project8_pi/Source.cpp
+++ Project8_pi/Project8_pi/Source.cpp
@@ -11,6 +11,8 @@ int main()
 
 	radius = 25.0f;
 
+	/* diceded to use a new libary to get the abiltiy to use powers rather than do radius* radius
+	this section finds the area by doing pi * radius squared */
 	area = pi * pow(radius, 2);
 
 	cout << "The area of a circle with a radius of " << radius << " is " << area << endl;
