7bd9fef4df1d091d9c084fd66fad29001046b41e
StBiccy <115167027+StBiccy@users.noreply.github.com>
2022-10-13 15:53:22 +0100

    Share of savings program
    math

---
+++ IfStatments/IfStatments/Source.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	bool a = false;
+
+	if (a)
+	{
+		num1++;
+	}
+
+}
---
+++ Program9_ShareOfSavings/Program9_ShareOfSavings/Source.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int mySavings = 2000;
+	int yourPercentage = 50;
+	int yourShare = (yourPercentage * (mySavings / 100));
+
+	cout << "your share: " << yourShare << endl;
+
+	return 0;
+}
