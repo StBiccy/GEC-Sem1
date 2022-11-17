afc558b7713ae1602cb4d3f346f38fd297d456c7
Moussa Azzoug <115167027+StBiccy@users.noreply.github.com>
2022-11-17 15:11:27 +0000

    Broken code
    i was taked to fix broken code which i did on accedent.
    
    ya know just a skill issue

---
+++ Program30_BrokenCode/Program30_BrokenCode/Source.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+int main()
+{
+	int num1 = 5;
+
+	for (int i = 0; i < num1; ++i)
+	{
+		std::cout << "i= " << i << std::endl;
+	}
+	int num2 = 15;
+
+	for (int i = 0; i < num2; ++i)
+	{
+		std::cout << "i= " << i << std::endl;
+	}
+
+	std::cin.get();
+		return 0;
+}
