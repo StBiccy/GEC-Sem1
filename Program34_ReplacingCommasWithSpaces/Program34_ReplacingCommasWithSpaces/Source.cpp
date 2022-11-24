0a64a76bfd72ba3e48dc2d95bf0e0c4a69e038d8
Moussa Azzoug <115167027+StBiccy@users.noreply.github.com>
2022-11-24 15:36:38 +0000

    i'll come back to program 34 in a bit

---
+++ Program34_ReplacingCommasWithSpaces/Program34_ReplacingCommasWithSpaces/Source.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+void comma2blank(char* textToChange);
+
+int main()
+{
+	char text[100];
+
+	cout << "Please input a sentence, but instead of spaces use commas." << endl;
+
+	cin.getline(text, 50);
+}
+
+void comma2blank(char* textToChange)
+{
+
+}
