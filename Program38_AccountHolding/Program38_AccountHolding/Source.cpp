ce49ec44d6d15787e796d26dacda9e78ae39fc22
Moussa Azzoug <115167027+StBiccy@users.noreply.github.com>
2022-12-11 11:16:04 +0000

    account holding completed with a only one issue

--- Program38_AccountHolding/Program38_AccountHolding/Source.cpp
+++ Program38_AccountHolding/Program38_AccountHolding/Source.cpp
@@ -18,11 +18,15 @@ int main()
 	bool while2;
 	int choice;
 	ofstream out_file;
-	ofstream in_file;
+	ifstream in_file;
+	int count = 0;
+	bool exists = false;
 
 	while (while1)
 	{
-		cout << "What would you like to do?\n 1: Enter new details\n 2: Display Contents of file\n 3: Display balances over 10,000\n 4: Exit program\n";
+		system("cls");
+
+		cout << "What would you like to do?\n 1: Enter new details\n 2: Display Contents of file\n 3: Display balances over 100,000\n 4: Exit program\n";
 		cin >> choice;
 		while2 = true;
 		switch (choice)
@@ -31,6 +35,7 @@ int main()
 			out_file.open("accounts", ios::out | ios::app);
 			while (while2)
 			{
+
 				cout << "Please entre a name: ";
 				cin >> acc1.name;
 				cout << "Please entre the account number: ";
@@ -41,7 +46,7 @@ int main()
 				out_file << acc1.accNum << endl;
 				out_file << acc1.bal << endl;
 
-
+				system("cls");
 				
 				while (true)
 				{
@@ -59,6 +64,8 @@ int main()
 					}
 					else
 					{
+						system("cls");
+
 						cout << "Invalid answer;" << endl;
 					}
 				}
@@ -67,11 +74,64 @@ int main()
 			break;
 
 		case 2:
+			system("cls");
 
+			in_file.open("accounts");
+			if (!in_file.eof())
+			{
+				count = 0;
+
+				while (!in_file.eof())
+				{
+					in_file >> acc2.name;
+					in_file >> acc2.accNum;
+					in_file >> acc2.bal;
+
+					++count;
+
+					cout << "\nAccout: " << count << endl << "Name on account: " << acc2.name << endl << "Account Number: " << acc2.accNum << endl<< "Balance: " << acc2.bal << endl;
+
+				}
+			}
+			else
+			{
+				cout << "File could not open!" << endl;
+			}
+			in_file.close();
+			system("pause");
 			break;
 
 		case 3:
+			in_file.open("accounts");
+			
+			if (!in_file.eof())
+			{
+				count = 0;
 
+				while (!in_file.eof())
+				{
+					in_file >> acc2.name;
+					in_file >> acc2.accNum;
+					in_file >> acc2.bal;
+
+					++count;
+
+					if (acc2.bal > 100000)
+					{
+						cout << "\nAccout: " << count << endl << "Name on account: " << acc2.name << endl << "Account Number: " << acc2.accNum << endl << "Balance: " << acc2.bal << endl;
+						exists = true;
+					}
+				}
+				if (exists == false)
+				{
+					cout << " No accounts over 100,000" << endl;
+				}
+			}
+			else
+			{
+				cout << "File failed to open" << endl;
+			}
+			system("pause");
 			break;
 
 		case 4:
