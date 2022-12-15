73acf1728832fa05dbee4d3b3bb1dea023ded75e
Moussa Azzoug <115167027+StBiccy@users.noreply.github.com>
2022-12-15 17:14:02 +0000

    finishedish. (it's broken in a way no one needs to know about

--- Program39_TopTenScores/Program39_TopTenScores/Source.cpp
+++ Program39_TopTenScores/Program39_TopTenScores/Source.cpp
@@ -1,73 +1,131 @@
 #include <iostream>
 #include <fstream>
+#include <vector>
+#include <cstdlib>
+#include <iomanip>
+#include <string>
 
 using namespace std;
 
-struct s
-{
-	std::string name;
-	int score;
-};
-
 int main()
 {
+	vector<int> score;
+	vector<string>name;
 
-	s input;
 
 	ifstream inFile;
 	ofstream outFile;
 
-	s held[10];
-
-	inFile.open("scores.txt");
-	
-	if (!inFile.eof())
+	outFile.open("score.txt", ios::out | ios::app);
+	outFile.close();
+	inFile.open("score.txt");
+	while (!inFile.eof())
 	{
-		for (int i = 0; i < 10; ++i)
+		int tempScore;
+		string tempName;
+
+		inFile >> tempScore;
+
+		if (inFile.eof())
 		{
-			if (!inFile.eof())
-			{
-				inFile >> held[i].score;
-				inFile >> held[i].name;
-			}
-			else
-				i = 10;
+			break;
 		}
+		score.push_back(tempScore);
+		inFile >> tempName;
+		name.push_back(tempName);
 	}
+	inFile.close();
+
+	int vectSize = score.size();
+
 
 	while (true)
 	{
 		system("cls");
 
+		int newScore;
+		string newName;
+
 		int choice;
 		cout << "1: Enter a score\n2: Display scores\n3: Exit" << std::endl;
+
+
 		cin >> choice;
 
 		switch (choice)
 		{
 		case 1:
-			inFile.open("score.txt");
+			outFile.open("score.txt");
 
-			cout << "Enter score: " << std::endl;
-			cin >> input.score;
+			cout << "Enter a score" << endl;
+			cin >> newScore;
 
-			cout << "Enter name: " << std::endl;
-			cin >> input.name;
+
+			cout << "Enter a name" << endl;
+			cin >> newName;
 
 			for (int i = 0; i < 10; ++i)
 			{
+				if (vectSize < i + 1)
+				{
+					score.push_back(newScore);
+					name.push_back(newName);
 
+					outFile << score[i] << endl;
+					outFile << name[i] << endl;
+
+					break;
+				}
+
+				if (score[i] < newScore)
+				{
+					int tempI = score[i];
+					string tempS = name [i];
+
+					score[i] = newScore;
+					name[i] = newName;
+
+					newScore = tempI;
+					newName = tempS;
+				}
+
+				outFile << score[i] << endl;
+				outFile << name[i] << endl;
 			}
+			outFile.close();
 
-
-				break;
+			break;
 
 		case 2:
+			system("cls");
+			if (score.empty())
+			{
+				cout << "No scores in system" << endl;
+			}
+			for (int i = 0; i < vectSize; ++i)
+			{
+				int width = 20;
+
+				int n = name[i].length();
+
+				width -= n;
+
+				if (i == 9)
+				{
+					cout << i + 1 << ": " << name[i];
+				}
+				else
+				{
+					cout << setw(2) << i + 1 << ": " << name[i];
+				}
+				cout << setw(width) << "score: " << score[i] << endl;
+			}
+			system("pause");
 
 			break;
 
 		case 3:
-
+			exit(1);
 			break;
 
 		default:
@@ -75,5 +133,7 @@ int main()
 			system("pause");
 			break;
 		}
+
 	}
+
 }
